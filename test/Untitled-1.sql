14aecec3e919cc8e72790d1164abd6d52f9705cc
LINPENG <823295965@QQ.COM>
2020-03-26 18:19:36 +0800

    CLEAR  ACHE

--- test/Untitled-1.sql
+++
@@ -1,7 +0,0 @@
-SELECT
- CONVERT( INT,DATENAME(DAY, savetime)),CONVERT(varchar, savetime, 20)
-FROM OneTwoRelation
-WHERE
-  CONVERT(varchar,savetime,20) like '%2020-01%';
-  SELECT *FROM OneTwoRelation;
-  SELECT * FROM RelationCode
